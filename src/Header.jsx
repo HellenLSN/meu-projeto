80d0e84ee9e1eccb03603a32f1353136e57eb755
HellenLSN <nogueira.hellen@escola.pr.gov.br>
2026-09-08 21:30:48 -0300

    08-09-2026 poke

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+export function Header() {
+  return (
+    <header className="pokedex-header">
+      <figure>
+        <img 
+          src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" 
+          alt="Logotipo Oficial PokéAPI" 
+        />
+      </figure>
+      <p className="pokedex-subtitulo">
+        Sua Enciclopédia Pokémon Interativa
+      </p>
+    </header>
+  );
+}
